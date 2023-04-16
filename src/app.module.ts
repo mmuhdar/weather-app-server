3cf417277ec78f8d85d805d585e13f228654e094
mmuhdar <mmuhdar17@gmail.com>
2023-04-16 13:28:35 +0800

    fix welcome page

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { HistoryModule } from './history/history.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [HistoryModule, PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule {}
