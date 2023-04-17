73b4ebf6a8852e38a02ac21d9c0dadd8ef95e500
mmuhdar <mmuhdar17@gmail.com>
2023-04-17 12:06:46 +0800

    fixing cors

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: ['https://weather-app-mmuhdar.vercel.app'],
-  });
+  // app.use((req, res, next) => {
+  //   res.header('Access-Control-Allow-Origin', '*');
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+  //   next();
+  // });
+  // app.enableCors({
+  //   allowedHeaders: '*',
+  //   origin: ['https://weather-app-mmuhdar.vercel.app', 'http://localhost:3000'],
+  // });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
