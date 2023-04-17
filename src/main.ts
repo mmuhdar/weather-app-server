b37e97a88ccb732ef7d86019cb4159cc048eb939
mmuhdar <mmuhdar17@gmail.com>
2023-04-17 12:13:49 +0800

    trying fixing cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
   const app = await NestFactory.create(AppModule);
   // app.use((req, res, next) => {
   //   res.header('Access-Control-Allow-Origin', '*');
@@ -14,7 +21,7 @@ async function bootstrap() {
   //   allowedHeaders: '*',
   //   origin: ['https://weather-app-mmuhdar.vercel.app', 'http://localhost:3000'],
   // });
-  app.enableCors();
+  app.enableCors(options);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
